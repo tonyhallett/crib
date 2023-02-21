ed2efae2a71442836cea76468633d60f5e2be8a2
Tony Hallett <tonyhallett74@gmail.com>
2023-02-21 11:02:40 +0000

    backup

--- Scorer.ts
+++ Scorer.ts
@@ -1,1 +1,35 @@
+enum Suit {Hearts , Clubs , Diamonds, Spades}
+enum Pips { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+interface Card {
+  suit : Suit,
+  pipss : Pips 
+}
 
+enum ScoreType { Pair, Three,Four, Flush, Run }
+interface Score{
+    type : ScoreType
+   cards  : Card []
+}
+type FiveCards = [Card, Card, Card, Card, Card ]
+type FourCards = [Card, Card, Card, Card ]
+type ThreeCards = [Card, Card, Card ]
+type Cards = Card[]
+type Flush = FourCards | FiveCards 
+//type Runs = FiveCards | FourCards | [FourCards, FourCards ] 
+interface Scores {
+    pairs : [Card, Card ][]
+    threes : ThreeCards | undefined 
+    fours : [FourCards | undefined ,
+    // nob 
+    runs : Cards [],
+    flush : Flush | undefined 
+}
+type ScoreCards  = [Card, Card,Card, Card,Card]
+function getScores(Cards:ScoreCards,topCard:Card,isBox : boolean ) : Score[] {
+ const scores:Score[] = [];
+
+}
+
+function getFlush(isBox:boolean):[Flush|undefined, isFullFlush ]{
+
+}
