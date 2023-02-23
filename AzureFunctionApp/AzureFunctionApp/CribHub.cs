0d84b9b6df024a2ada28619fe0e274e80ba05fad
Tony Hallett <tonyhallett74@gmail.com>
2023-02-23 18:03:49 +0000

    Add azure app

---
+++ AzureFunctionApp/AzureFunctionApp/CribHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace AzureFunctionApp
+{
+    public static class CribHub
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "HubValue")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
