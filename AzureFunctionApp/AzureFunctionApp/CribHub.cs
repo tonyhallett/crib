e96eb535c79b8f99d4472538a58ecdca7ee69328
Tony Hallett <tonyhallett74@gmail.com>
2023-02-24 13:17:15 +0000

    backup

--- AzureFunctionApp/AzureFunctionApp/CribHub.cs
+++ AzureFunctionApp/AzureFunctionApp/CribHub.cs
@@ -1,10 +1,59 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Azure.SignalR.Management;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AzureFunctionApp
 {
+    public class TestServiceHubContext : ServiceHubContext<ICribClient>
+    {
+        public override IHubClients<ICribClient> Clients => throw new System.NotImplementedException();
+
+        // abstract class
+        public override GroupManager Groups => throw new System.NotImplementedException();
+
+        // abstract class
+        public override UserGroupManager UserGroups => throw new System.NotImplementedException();
+
+        // abstract class
+        public override ClientManager ClientManager => throw new System.NotImplementedException();
+
+        public override void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ValueTask<NegotiationResponse> NegotiateAsync(NegotiationOptions negotiationOptions = null, CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public interface ICribClient
+    {
+        // method need to be Tasks / void
+    }
+    //InvocationContext has ConnectId and UserId
+    public class CribHub2 : ServerlessHub<ICribClient>
+    {
+        public CribHub2() { }
+
+        // testing - ServiceHubContext<T> is abstract
+        public CribHub2(ServiceHubContext<ICribClient> context) : base(context)
+        {
+            
+        }
+    }
+
     public static class CribHub
     {
         [FunctionName("negotiate")]
@@ -12,6 +61,7 @@ namespace AzureFunctionApp
             [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
             [SignalRConnectionInfo(HubName = "HubValue")] SignalRConnectionInfo connectionInfo)
         {
+            var x = new ServerlessHub<IHubClient>()
             return connectionInfo;
         }
     }
