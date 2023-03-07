ef97170b192f657e4584922205fd779e6876eda2
Tony Hallett <tonyhallett74@gmail.com>
2023-03-07 18:27:45 +0000

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
 import * as signalR from '@microsoft/signalr';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { clientFactory, CribHub, hubFactory} from './generatedTypes'
+// https://learn.microsoft.com/en-us/javascript/api/@microsoft/signalr/hubconnection?view=signalr-js-latest
+
+interface ClientPrincipal {
+    identityProvider: string;
+    userDetails: string;
+
+}
+
+const NavBar = (props:{user:ClientPrincipal}) => { 
+    const {user} = props;
+    // to change to provider type
+    const providers = ['twitter', 'github', 'aad']; 
+    /*
+        Check OneNote as there is another
+        <a href="/.auth/login/aad">Log in with the Microsoft Identity Platform</a>
+        <a href="/.auth/login/facebook">Log in with Facebook</a>
+        <a href="/.auth/login/google">Log in with Google</a>
+        <a href="/.auth/login/twitter">Log in with Twitter</a>
+        <a href="/.auth/login/apple">Log in with Apple</a>
+
+    */
+    const redirect = `/`; 
+
+    return ( 
+        <> 
+        {!user && providers.map((provider) => ( 
+            <span><a key={provider} href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}><h4>{provider}</h4></a> </span> 
+        ))} 
+
+        {user && ( 
+            <div> 
+                <p> 
+                    <span>{user.userDetails + " " +  user.identityProvider}</span> 
+
+                    <span> <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}> 
+                        Logout 
+                    </a> 
+                    </span> 
+                </p> 
+            </div> 
+        )} 
+        </> 
+    ) 
+} 
+
+
 
 export default function App(){
     const ref = useRef<CribHub|undefined>(undefined);
+    const [isAuthenticated, setIsAuthenticated] = useState(false); 
+    const [user, setUser] = useState<ClientPrincipal|undefined>(); 
     const [connected, setConnected] = useState(false);
     const [receivedBroadcast, setReceivedBroadcast] = useState("");
     const sendMessageClickHandler = useCallback(async () =>{
@@ -12,35 +60,57 @@ export default function App(){
         }catch(e){
             console.log("Error broadcasting")
         }
+    },[]);
+    const signalRConnectClickHandler = useCallback(async () =>{
+        //todo store the connection
+        const connection = new signalR.HubConnectionBuilder().withUrl("todo").build();
+        const cribConnection = clientFactory.crib(connection, {
+            receivedBroadcast(message, clientid, claims) {
+                setReceivedBroadcast(`${message} - ${clientid} - ${claims}`);
+            },
+        });
+        
+        const serverProxy = hubFactory.crib(connection);
+        ref.current = serverProxy;
+        try{
+            await connection.start();
+            setConnected(true);
+        }catch(e){
+            // retry logic 
+        }
     },[])
     // automatic reconnect ?
     // logging
 
     // will probably use redux
     useEffect(() => {
-        // https://learn.microsoft.com/en-us/javascript/api/@microsoft/signalr/hubconnection?view=signalr-js-latest
-        const signalRConnect = async () => {
-            const connection = new signalR.HubConnectionBuilder().withUrl("todo").build();
-            clientFactory.crib(connection, {
-                receivedBroadcast(message, clientid, claims) {
-                    setReceivedBroadcast(`${message} - ${clientid} - ${claims}`);
-                },
-            });
-            const serverProxy = hubFactory.crib(connection);
-            ref.current = serverProxy;
-            try{
-                await connection.start();
-                setConnected(true);
-            }catch(e){
-                // retry logic 
-            }
-        }
+        
+        async function getUserInfo() { 
+            try { 
+                // need the full path - should import config from another ts file that do not upload to 
+                // public github - add environment variables and have parcel do the rest
+                const response = await fetch('/.auth/me'); 
+                const payload = await response.json(); 
+                const { clientPrincipal } = payload; 
+                if(clientPrincipal){ 
+                  setUser(clientPrincipal); 
+                  setIsAuthenticated(true); 
+                  console.log(`clientPrincipal = ${JSON.stringify(clientPrincipal)}`); 
+                }  
+            } catch (error:any) { 
+                console.error('No profile could be found ' + error?.message?.toString()); 
+            } 
+        
+        };
 
+        getUserInfo();
     });
 
-    
+    const canSignalRConnect = isAuthenticated && !connected;
 
     return <div>
+
+        <button disabled={!canSignalRConnect} onClick={signalRConnectClickHandler}>SignalR connect</button>
         <button onClick={sendMessageClickHandler} disabled={!connected}>Send hello to SignalR service</button>
         <div>{receivedBroadcast}</div>
     </div>;
