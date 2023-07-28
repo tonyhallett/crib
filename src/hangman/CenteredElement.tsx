a8bb010d69107d658597a1981c9559cd4ff8b1f2
Tony Hallett <tonyhallett74@gmail.com>
2023-07-28 11:38:28 +0100

    backup

--- src/hangman/CenteredElement.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export function CenteredElement(props: { children: React.ReactNode }) {
-  return (
-    <Box display="flex" justifyContent="center">
-      {props.children}
-    </Box>
-  );
-}
